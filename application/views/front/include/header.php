018e136ed939c1689f924c3bf08578d8b4271616
powerremit <powerremit@naver.com>
2024-06-12 16:55:19 +0900

    - 드랍다운메뉴

--- application/views/front/include/header.php
+++ application/views/front/include/header.php
@@ -10,12 +10,47 @@
         <div class="top-nav s-12 l-9">
             <ul class="top-ul right chevron">
                 <li><a href="/">Hanol Education</a></li>
-                <li><a href="/history">History</a></li>
-                <li><a href="/studyinkorea">Study in Korea</a></li>
-                <li><a href="/university">Universities</a></li>
-                <li><a href="/gallery">KUFS</a></li>
+                <li>
+                    <a href="/history">History</a>
+                    <ul class="dropdown">
+                        <li><a href="/subpage5">Vietnam</a></li>
+                        <li><a href="/subpage6">Mongolia</a></li>
+                        <li><a href="/subpage6">Iran</a></li>
+                        <li><a href="/subpage6">Malaysia</a></li>
+                        <li><a href="/subpage6">Indonesia</a></li>
+                        <li><a href="/subpage6">Pakistan</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="/studyinkorea">Study in Korea</a>
+                    <ul class="dropdown">
+                        <li><a href="/subpage5">Curriculums</a></li>
+                        <li><a href="/subpage6">Process</a></li>
+                        <li><a href="/subpage6">Documents</a></li>
+                    </ul>
+                </li>
+<!--                <li><a href="/university">Universities</a></li>-->
+                <li>
+                    <a href="/gallery">KUFS</a>
+                    <ul class="dropdown">
+                        <li><a href="/subpage5">KUFS Vietnam</a></li>
+                        <li><a href="/subpage6">KUFS Mongolia</a></li>
+                    </ul></li>
+
                 <li><a href="/contact">Contact</a></li>
             </ul>
         </div>
     </nav>
 </header>
+
+<style>
+    .dropdown {
+        display: none;
+        position: absolute;
+        background-color: white;
+        /*min-width: ;*/
+        box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+        z-index: 1;
+    }
+
+</style>
