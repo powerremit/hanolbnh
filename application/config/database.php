4d30c5b999a3dce9d19a17c61ec97ff09060893c
powerremit <powerremit@naver.com>
2024-06-19 12:01:22 +0900

    - 인덱스

--- application/config/database.php
+++ application/config/database.php
@@ -73,16 +73,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
-$db['default'] = array(
+$db['local'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => '',
-	'password' => '',
-	'database' => '',
+	'hostname' => '127.0.0.1:3306',
+	'username' => 'powerremit',
+	'password' => 'qkdwldnjs',
+	'database' => 'hanol',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
-	'db_debug' => (ENVIRONMENT !== 'production'),
+//	'db_debug' => (ENVIRONMENT !== 'production'),
 	'cache_on' => FALSE,
 	'cachedir' => '',
 	'char_set' => 'utf8',
@@ -93,4 +93,48 @@ $db['default'] = array(
 	'stricton' => FALSE,
 	'failover' => array(),
 	'save_queries' => TRUE
+);
+
+$db['dev'] = array(
+    'dsn'	=> '',
+    'hostname' => '127.0.0.1:3306',
+    'username' => 'powerremit',
+    'password' => 'qkdwldnjs',
+    'database' => 'hanol',
+    'dbdriver' => 'mysqli',
+    'dbprefix' => '',
+    'pconnect' => FALSE,
+//    'db_debug' => (ENVIRONMENT !== 'production'),
+    'cache_on' => FALSE,
+    'cachedir' => '',
+    'char_set' => 'utf8',
+    'dbcollat' => 'utf8_general_ci',
+    'swap_pre' => '',
+    'encrypt' => FALSE,
+    'compress' => FALSE,
+    'stricton' => FALSE,
+    'failover' => array(),
+    'save_queries' => TRUE
+);
+
+$db['prod'] = array(
+    'dsn'	=> '',
+    'hostname' => '127.0.0.1:3306',
+    'username' => 'powerremit',
+    'password' => 'qkdwldnjs',
+    'database' => 'hanol',
+    'dbdriver' => 'mysqli',
+    'dbprefix' => '',
+    'pconnect' => FALSE,
+//    'db_debug' => (ENVIRONMENT !== 'production'),
+    'cache_on' => FALSE,
+    'cachedir' => '',
+    'char_set' => 'utf8',
+    'dbcollat' => 'utf8_general_ci',
+    'swap_pre' => '',
+    'encrypt' => FALSE,
+    'compress' => FALSE,
+    'stricton' => FALSE,
+    'failover' => array(),
+    'save_queries' => TRUE
 );
--- index.php
+++ index.php
@@ -53,7 +53,7 @@
  *
  * NOTE: If you change these, also change the error_reporting() code below
  */
-	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
+	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'local');
 
 /*
  *---------------------------------------------------------------
@@ -65,23 +65,30 @@
  */
 switch (ENVIRONMENT)
 {
-	case 'development':
-		error_reporting(-1);
-		ini_set('display_errors', 1);
-	break;
+    case 'local' :
+        define('_TYPE', 'local');
+        error_reporting(-1);
+        ini_set('display_errors', 1);
+        break;
+    case 'dev':
+        define('_TYPE', 'dev');
+        error_reporting(-1);
+        ini_set('display_errors', 1);
+        break;
 
-	case 'testing':
-	case 'production':
-		ini_set('display_errors', 0);
-		if (version_compare(PHP_VERSION, '5.3', '>='))
-		{
-			error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
-		}
-		else
-		{
-			error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE);
-		}
-	break;
+    case 'testing':
+    case 'prod':
+        define('_TYPE', 'prod');
+        ini_set('display_errors', 0);
+        if (version_compare(PHP_VERSION, '5.3', '>='))
+        {
+            error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
+        }
+        else
+        {
+            error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE);
+        }
+        break;
 
 	default:
 		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
